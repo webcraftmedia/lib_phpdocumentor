fe1c3909426881fd10c55b688e0c48aa8acb9f13
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-03-04 17:44:15 +0100

    #2 more options for phpdoc generation wrapper class

--- lib/phpdocumentor.php
+++ lib/phpdocumentor.php
@@ -1,6 +1,15 @@
 <?php
 class phpdocumentor {
-    public static function run($inpath,$outpath){
-        shell_exec(dirname(__FILE__).'/phpDocumentor.phar run -d '.$inpath.' -t '.$outpath);
+    public static function run($inpath,$outpath,$cachepath,$title,$sourcecode,$parseprivate){
+        $sourcecode_ = $sourcecode ? '--sourcecode ' : '';
+        $parseprivate_ = $parseprivate ? '--parseprivate ' : '';
+        new INFO(shell_exec(dirname(__FILE__).'/phpDocumentor.phar run'.
+                            ' -d '.$inpath.
+                            ' -t '.$outpath.
+                            ' --cache-folder '.$cachepath.
+                            ' '.$sourcecode_.
+                            ' '.$parseprivate_.
+                            ' --title "'.$title.'" 2>&1'));
+        return \SYSTEM\LOG\JsonResult::ok();
     }
 }
