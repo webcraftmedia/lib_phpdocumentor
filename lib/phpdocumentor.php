33be5be1b0f01423dee08ddb1091599f92e106ad
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-03-04 19:19:27 +0100

    #2 ignore paths, more infos

--- lib/phpdocumentor.php
+++ lib/phpdocumentor.php
@@ -1,12 +1,26 @@
 <?php
 class phpdocumentor {
-    public static function run($inpath,$outpath,$cachepath,$title,$sourcecode,$parseprivate){
+    public static function run($inpath,$outpath,$cachepath,$ignore,$title,$sourcecode,$parseprivate){
         $sourcecode_ = $sourcecode ? '--sourcecode ' : '';
         $parseprivate_ = $parseprivate ? '--parseprivate ' : '';
+        $ignore_ = '';
+        foreach($ignore as $path){
+            $ignore_ .= $path.',';}
+        if($ignore_ != ''){
+            $ignore_ = '--ignore '.rtrim($ignore_, ',');}
+        new INFO(dirname(__FILE__).'/phpDocumentor.phar run'.
+                            ' -d '.$inpath->SERVERPATH().
+                            ' -t '.$outpath->SERVERPATH().
+                            ' --cache-folder '.$cachepath->SERVERPATH().
+                            ' '.$ignore_.
+                            ' '.$sourcecode_.
+                            ' '.$parseprivate_.
+                            ' --title "'.$title.'" 2>&1');
         new INFO(shell_exec(dirname(__FILE__).'/phpDocumentor.phar run'.
-                            ' -d '.$inpath.
-                            ' -t '.$outpath.
-                            ' --cache-folder '.$cachepath.
+                            ' -d '.$inpath->SERVERPATH().
+                            ' -t '.$outpath->SERVERPATH().
+                            ' --cache-folder '.$cachepath->SERVERPATH().
+                            ' '.$ignore_.
                             ' '.$sourcecode_.
                             ' '.$parseprivate_.
                             ' --title "'.$title.'" 2>&1'));
