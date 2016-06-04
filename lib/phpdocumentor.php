f0c4f33ba8bfb1904b424e1611d93974d38aa808
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-06-04 23:44:06 +0200

    updated phpdocumentor to use php5

--- lib/phpdocumentor.php
+++ lib/phpdocumentor.php
@@ -8,21 +8,14 @@ class phpdocumentor {
             $ignore_ .= $path.',';}
         if($ignore_ != ''){
             $ignore_ = '--ignore '.rtrim($ignore_, ',');}
-        new INFO(dirname(__FILE__).'/phpDocumentor.phar run'.
+        new INFO(shell_exec('php5 '.dirname(__FILE__).'/phpDocumentor.phar run'.
                             ' -d '.$inpath->SERVERPATH().
                             ' -t '.$outpath->SERVERPATH().
                             ' --cache-folder '.$cachepath->SERVERPATH().
                             ' '.$ignore_.
                             ' '.$sourcecode_.
                             ' '.$parseprivate_.
-                            ' --title "'.$title.'" 2>&1');
-        new INFO(shell_exec(dirname(__FILE__).'/phpDocumentor.phar run'.
-                            ' -d '.$inpath->SERVERPATH().
-                            ' -t '.$outpath->SERVERPATH().
-                            ' --cache-folder '.$cachepath->SERVERPATH().
-                            ' '.$ignore_.
-                            ' '.$sourcecode_.
-                            ' '.$parseprivate_.
+                            ' --force'.
                             ' --title "'.$title.'" 2>&1'));
         return \SYSTEM\LOG\JsonResult::ok();
     }
